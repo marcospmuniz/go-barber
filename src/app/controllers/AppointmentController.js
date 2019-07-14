0c26c3df90467adc98e4eef1dfd5dcf44387724c
Marcos de P. Muniz <marcospmuniz@gmail.com>
2019-07-14 16:56:51 -0300

    feature: add notificações de novos agendamentos para os providers usando o mongodb

--- src/app/controllers/AppointmentController.js
+++ src/app/controllers/AppointmentController.js
@@ -1,8 +1,12 @@
 import * as Yup from 'yup';
-import { startOfHour, parseISO, isBefore } from 'date-fns';
+import {
+  startOfHour, parseISO, isBefore, format,
+} from 'date-fns';
+import pt from 'date-fns/locale/pt-BR';
 import User from '../models/User';
 import File from '../models/File';
 import Appointment from '../models/Appointment';
+import Notification from '../schemas/Notifications';
 
 class AppointmentController {
   async index(req, res) {
@@ -91,6 +95,18 @@ class AppointmentController {
       user_id: req.userId,
       provider_id,
       date,
+    });
+
+    /**
+     * Notify provider
+     */
+    const user = await User.findByPk(req.userId);
+
+    const formatedDate = format(hourStart, "dd 'de' MMMM', às' H:mm", { locale: pt });
+
+    await Notification.create({
+      content: `Novo agendamento de ${user.name} para o dia ${formatedDate}h`,
+      user: provider_id,
     });
 
     return res.json(appointment);
