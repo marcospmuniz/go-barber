3380675513027525e8474ef7ce83ecc208a05dbc
Marcos de P. Muniz <marcospmuniz@gmail.com>
2019-07-14 14:43:30 -0300

    feature: validações dos agedamentos pela data

--- src/app/controllers/AppointmentController.js
+++ src/app/controllers/AppointmentController.js
@@ -1,4 +1,5 @@
 import * as Yup from 'yup';
+import { startOfHour, parseISO, isBefore } from 'date-fns';
 import User from '../models/User';
 import Appointment from '../models/Appointment';
 
@@ -23,7 +24,35 @@ class AppointmentController {
     });
 
     if (!isProvider) {
-      return res.status(401).json({ error: 'You can only create appointments with providers' });
+      return res.status(401).json({
+        error: 'You can only create appointments with providers',
+      });
+    }
+
+    /**
+     * Check for past dates
+     */
+    // despresa os minutos de "date" e retorna uma data apenas com a hora cheia de date
+    const hourStart = startOfHour(parseISO(date));
+
+    // checa se a hourStart está no passado
+    if (isBefore(hourStart, new Date())) {
+      return res.status(400).json({ error: 'Past dates are not permitted!' });
+    }
+
+    /**
+     * Check date availability
+     */
+    const checkAvailability = await Appointment.findOne({
+      where: {
+        provider_id,
+        canceled_at: null,
+        date: hourStart,
+      },
+    });
+
+    if (checkAvailability) {
+      return res.status(400).json({ error: 'Appointment date not available!' });
     }
 
     const appointment = await Appointment.create({
